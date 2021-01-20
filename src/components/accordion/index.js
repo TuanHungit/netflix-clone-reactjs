8fe89d81e0ac03884634766422960cb3d76290eb
NguyenTuanHung14 <17110156@student.hcmute.edu.vn>
2021-01-20 23:31:27 +0700

    add fags component

---
+++ src/components/accordion/index.js
@@ -0,0 +1,54 @@
+import React, { createContext, useState, useContext } from 'react';
+
+import {
+  Container,
+  Frame,
+  Inner,
+  Item,
+  Title,
+  Header,
+  Body,
+} from './style/accordion';
+
+const ToggleContext = createContext();
+export default function Accordion({ children, ...restProps }) {
+  return (
+    <Container>
+      <Inner>{children}</Inner>
+    </Container>
+  );
+}
+Accordion.Item = function AccordionItem({ children, ...restProps }) {
+  const [toggleShow, setToggleShow] = useState(false);
+  return (
+    <ToggleContext.Provider value={{ toggleShow, setToggleShow }}>
+      <Item {...restProps}>{children}</Item>
+    </ToggleContext.Provider>
+  );
+};
+
+Accordion.Title = ({ children, ...restProps }) => {
+  return <Title {...restProps}>{children}</Title>;
+};
+Accordion.Frame = ({ children, ...restProps }) => {
+  return <Frame {...restProps}>{children}</Frame>;
+};
+
+Accordion.Header = function AccordionHeader({ children, ...restProps }) {
+  const { toggleShow, setToggleShow } = useContext(ToggleContext);
+  return (
+    <Header onClick={() => setToggleShow((prev) => !prev)} {...restProps}>
+      {children}
+      {toggleShow ? (
+        <img src="/images/icons/close-slim.png" alt="Close" />
+      ) : (
+        <img src="/images/icons/add.png" alt="Open" />
+      )}
+    </Header>
+  );
+};
+
+Accordion.Body = function AccordionBody({ children, ...restProps }) {
+  const { toggleShow } = useContext(ToggleContext);
+  return toggleShow ? <Body {...restProps}>{children}</Body> : null;
+};
