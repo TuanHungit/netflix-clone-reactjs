1e73ee72018aea0036f458aec2fd16166bab987b
NguyenTuanHung14 <17110156@student.hcmute.edu.vn>
2021-01-24 17:22:09 +0700

    created search input

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -3,7 +3,8 @@ import React, { useContext, useState, useEffect } from 'react';
 import { Loading, Header } from '../components';
 import ProfileContainer from './profiles';
 import { FirebaseContext } from '../context/firebase';
-
+import logo from '../logo.svg';
+import * as ROUTES from '../constrains/routes';
 export default function Browse({ slides }) {
   const { firebase } = useContext(FirebaseContext);
   const [loading, setLoading] = useState(true);
@@ -18,10 +19,47 @@ export default function Browse({ slides }) {
 
   return profile.displayName ? (
     <>
-      {' '}
       {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
       <Header src="joker1">
-        <p>Hello</p>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+            <Header.TextLink>Series</Header.TextLink>
+            <Header.TextLink>Films</Header.TextLink>
+          </Header.Group>
+
+          <Header.Group>
+            <Header.Group>
+              <Header.Search>
+                <Header.SearchIcon></Header.SearchIcon>
+                <Header.SearchInput></Header.SearchInput>
+              </Header.Search>
+            </Header.Group>
+            <Header.Profile>
+              <Header.Picture src={user.photoURL} />
+              <Header.Dropdown>
+                <Header.Group>
+                  <Header.Picture src={user.photoURL} />
+                  <Header.TextLink>{user.displayName}</Header.TextLink>
+                </Header.Group>
+                <Header.Group>
+                  <Header.TextLink>Sign out</Header.TextLink>
+                </Header.Group>
+              </Header.Dropdown>
+            </Header.Profile>
+          </Header.Group>
+        </Header.Frame>
+        <Header.Feature>
+          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+          <Header.Text>
+            Forever alone in a crowd, failed comedian Arthur Fleck seeks
+            connection as he walks the streets of Gotham City. Arthur wears two
+            masks -- the one he paints for his day job as a clown, and the guise
+            he projects in a futile attempt to feel like he's part of the world
+            around him.
+          </Header.Text>
+          <Header.PlayButton>Play</Header.PlayButton>
+        </Header.Feature>
       </Header>
     </>
   ) : (
--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -10,25 +10,24 @@ export default function SelectProfileContainer({ user, setProfile }) {
         <Header.Frame>
           <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
         </Header.Frame>
+        <Profiles>
+          <Profiles.Title>Who's watching?</Profiles.Title>
+          <Profiles.List>
+            <Profiles.User
+              onClick={() =>
+                setProfile({
+                  displayName: user.displayName,
+                  photoURL: user.photoURL,
+                })
+              }
+              data-testid="user-profile"
+            >
+              <Profiles.Picture src={user.photoURL} />
+              <Profiles.Name>{user.displayName}</Profiles.Name>
+            </Profiles.User>
+          </Profiles.List>
+        </Profiles>
       </Header>
-
-      <Profiles>
-        <Profiles.Title>Who's watching?</Profiles.Title>
-        <Profiles.List>
-          <Profiles.User
-            onClick={() =>
-              setProfile({
-                displayName: user.displayName,
-                photoURL: user.photoURL,
-              })
-            }
-            data-testid="user-profile"
-          >
-            <Profiles.Picture src={user.photoURL} />
-            <Profiles.Name>{user.displayName}</Profiles.Name>
-          </Profiles.User>
-        </Profiles.List>
-      </Profiles>
     </>
   );
 }
